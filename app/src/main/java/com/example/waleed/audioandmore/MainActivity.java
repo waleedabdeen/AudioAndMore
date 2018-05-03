3d25b4c22f550c9e56018697b684ba7702e242eb
Weeds <waleed.totti@gmail.com>
2018-05-03 12:25:48 +0200

    Link main activity to record activity

--- app/src/main/java/com/example/waleed/audioandmore/MainActivity.java
+++ app/src/main/java/com/example/waleed/audioandmore/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.waleed.audioandmore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnRecordActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRecordActivity = findViewById(R.id.btnRecordActivity);
+
+        btnRecordActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent (MainActivity.this, RecordActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
