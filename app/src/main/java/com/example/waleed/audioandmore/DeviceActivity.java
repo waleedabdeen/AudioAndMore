e00015c1372f562887974212091ed628c4aa0a36
Unknown <122371551@qq.com>
2018-05-07 16:08:04 +0800

    Device detail layout

---
+++ app/src/main/java/com/example/waleed/audioandmore/DeviceActivity.java
@@ -0,0 +1,13 @@
+package com.example.waleed.audioandmore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device);
+    }
+}
